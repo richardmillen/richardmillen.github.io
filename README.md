9cb65c4ba4cddfa871c41b423e57fb02014250ce
richardmillen <richardjmillen@gmail.com>
2021-02-15 23:12:42 +0000

    Initial commit. Minimal BJS scene, with bare-bones glb-imported mesh.
    Manually copied from build output of mysite repo.

---
+++ README.md
@@ -0,0 +1,2 @@
+# mysite
+richardmillen.github.io
---
+++ passage/index.js
@@ -0,0 +1,17 @@
+import BABYLON from "../_snowpack/pkg/babylonjs.js";
+import "../_snowpack/pkg/babylonjs-loaders.js";
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+const scene = new BABYLON.Scene(engine);
+const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.1, 20, BABYLON.Vector3.Zero(), scene);
+camera.attachControl(true);
+const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+BABYLON.SceneLoader.ImportMeshAsync("", "/meshes/", "passage-v1.glb").then(() => {
+  console.log("glb loaded!");
+});
+engine.runRenderLoop(() => {
+  scene.render();
+});
+window.addEventListener("resize", () => {
+  engine.resize();
+});
